cb5c8c705fb733c913631a7553060222911e539c
Nils Nahooy <nils.nahooy@live.com>
2023-10-13 15:29:36 +0200

    Add dialog

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,19 @@
-﻿namespace MusicStore.ViewModels;
+﻿using System.Reactive.Linq;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace MusicStore.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public ICommand BuyMusicCommand { get; }
+    public Interaction<MusicStoreViewModel, AlbumViewModel?> ShowDialog { get; }
+
+    public MainWindowViewModel() {
+        ShowDialog = new();
+        BuyMusicCommand = ReactiveCommand.CreateFromTask(async() => {
+            MusicStoreViewModel store = new();
+            var result = await ShowDialog.Handle(store);
+        });
+    }
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,11 +1,25 @@
-using Avalonia.Controls;
+using System.Threading.Tasks;
+using Avalonia.ReactiveUI;
+using MusicStore.ViewModels;
+using ReactiveUI;
 
 namespace MusicStore.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
 {
+    private async Task DoShowDialogAsync(InteractionContext<MusicStoreViewModel, AlbumViewModel?> interaction)
+{
+        var dialog = new MusicStoreWindow
+        {
+            DataContext = interaction.Input
+        };
+
+        var result = await dialog.ShowDialog<AlbumViewModel?>(this);
+     interaction.SetOutput(result);
+}
     public MainWindow()
     {
         InitializeComponent();
+        this.WhenActivated(action => action(ViewModel!.ShowDialog.RegisterHandler(DoShowDialogAsync)));
     }
 }
