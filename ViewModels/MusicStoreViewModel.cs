b099afedeb0acfd34b20bd3b0d1cd1ef7feb8ecc
Nils Nahooy <nils.nahooy@live.com>
2023-10-13 20:09:51 +0200

    set up view and controls

--- ViewModels/MusicStoreViewModel.cs
+++ ViewModels/MusicStoreViewModel.cs
@@ -1,6 +1,20 @@
+using ReactiveUI;
+
 namespace MusicStore.ViewModels;
 
 public class MusicStoreViewModel : ViewModelBase
 {
+    private string? _searchString;
+    private bool _isBusy;
+
+    public string? SearchString {
+        get => _searchString;
+        set => this.RaiseAndSetIfChanged(ref _searchString, value);
+    }
+
+    public bool IsBusy {
+        get => _isBusy;
+        set => this.RaiseAndSetIfChanged(ref _isBusy, value);
+    }
 
 }
