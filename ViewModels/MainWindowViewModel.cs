409c757bbda11a867f9f09c80bc5504c70410240
Nils Nahooy <nils.nahooy@live.com>
2023-10-17 20:02:36 +0200

    add items to collection

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Reactive.Linq;
+﻿using System.Collections.ObjectModel;
+using System.Reactive.Linq;
 using System.Windows.Input;
 using ReactiveUI;
 
@@ -8,12 +9,17 @@ public class MainWindowViewModel : ViewModelBase
 {
     public ICommand BuyMusicCommand { get; }
     public Interaction<MusicStoreViewModel, AlbumViewModel?> ShowDialog { get; }
+    public ObservableCollection<AlbumViewModel> Albums { get; } = new();
 
     public MainWindowViewModel() {
         ShowDialog = new();
         BuyMusicCommand = ReactiveCommand.CreateFromTask(async() => {
             MusicStoreViewModel store = new();
             var result = await ShowDialog.Handle(store);
+            if(result != null)
+            {
+                Albums.Add(result);
+            }
         });
     }
 }
